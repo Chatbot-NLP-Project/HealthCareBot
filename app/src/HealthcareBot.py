e3f89084d2284ae8d30deb6b2413003f9d8da56a
Sandaruwan Kumarasinghe <sandaruwan.18@cse.mrt.ac.lk>
2021-11-04 14:58:59 +0530

    change data

--- app/src/HealthcareBot.py
+++ app/src/HealthcareBot.py
@@ -82,7 +82,7 @@ model = tflearn.DNN(net)
 # model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
 # model.save("model.tflearn")
 
-
+#
 model.load("model.tflearn")
 
 def bag_of_words(s, words):
